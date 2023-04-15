7f07cb010dc882afc6dae0530cdd514f5b08d8bc
Vincent Chevalier <chev0100@algonquinlive.com>
2023-04-15 18:18:36 -0400

    built usesessionstorage hook

--- src/context/TokenContext.jsx
+++ src/context/TokenContext.jsx
@@ -1,20 +1,22 @@
 import { useContext, createContext, useState, useEffect } from 'react';
+import useSessionStorage from '../hooks/UseSessionStorage';
 
 const TokenContext = createContext();
 
 function TokenProvider(props) {
-  const [token, setToken] = useState(null);
   const tokenKey = import.meta.env.VITE_TOKEN_KEY;
+  const [token, setToken] = useSessionStorage(tokenKey, null);
 
   useEffect(() => {
     //TODO: actually add the sessionStorage code
+    useSessionStorage(tokenKey, token);
     //check in sessionStorage
     //for an existing value
     //handle removeItem if token is null
     // get code from context assignment
-  }, [token]);
+  }, [token, tokenKey]);
 
-  return <TokenContext.Provider value={[token, setToken]} {...props} />;
+  return <TokenContext.Provider value={[token, useToken]} {...props} />;
 }
 
 function useToken() {
