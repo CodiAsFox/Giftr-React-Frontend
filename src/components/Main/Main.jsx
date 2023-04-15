c883b85ac217e4baf82ef58203887e67486e1ce6
Tay <rami0101@algonquinlive.com>
2023-04-15 19:45:41 -0400

    Added Home Component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,29 +1,27 @@
-import { Route, Routes} from 'react-router-dom';
-import Login from '../Login/Login'
-import People from '../People/People'
-import Person from '../Person/Person'
-import Gifts from '../Gifts/Gifts'
-import Gift from '../Gift/Gift'
-import NotFound from '../NotFound/NotFound'
-import './Main.css';
-
+import { Route, Routes } from "react-router-dom";
+import Home from "../Home/Home";
+import People from "../People/People";
+import Person from "../Person/Person";
+import Gifts from "../Gifts/Gifts";
+import Gift from "../Gift/Gift";
+import NotFound from "../NotFound/NotFound";
+import "./Main.css";
 
 const Main = () => {
   return (
     <main>
       <Routes>
-        <Route path="/" element={<Login/>}  index/>
-        <Route path="/people/" element={<People/>}>
-          <Route path=":id" element={<Person/>}>
-          </Route>
+        <Route path="/" element={<Home />} index />
+        <Route path="/people/" element={<People />}>
+          <Route path=":id" element={<Person />}></Route>
         </Route>
-        <Route path="/gifts/" element={<Gifts/>}>
-          <Route path=":giftId" element={<Gift/>}/>
+        <Route path="/gifts/" element={<Gifts />}>
+          <Route path=":giftId" element={<Gift />} />
         </Route>
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
