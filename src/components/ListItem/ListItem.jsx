31f1cebcb0cf45e741593178a84cc32ea4e83a2c
Vincent Chevalier <chev0100@algonquinlive.com>
2023-04-16 19:07:05 -0400

    added person details

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,9 +1,22 @@
-const ListItem = () => {
-  return (
-    <li>
-      
-    </li>
-  )
+import { NavLink } from 'react-router-dom';
+
+const ListItem = (props) => {
+  const person = props.person;
+
+  if(person) {
+    return (
+      <li>
+        <div>
+          <p className="person-name">{person.name}</p>
+          <p className="person-dob">{person.dob}</p>
+        </div>
+        <div className="actions">
+          <NavLink to={`/people/${person.id}`}>Edit</NavLink>
+          <NavLink to={`/gifts/`}>Gifts</NavLink>
+        </div>
+      </li>
+    )
+  }
 }
 
-export default ListItem
+export default ListItem;
