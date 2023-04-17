5826a1314f9f4af991337e65f7aa48d13d8cee85
Vincent Chevalier <chev0100@algonquinlive.com>
2023-04-17 12:37:22 -0400

    switched to Giftr context

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -6,10 +6,11 @@ import ListItem from '../ListItem/ListItem';
 
 const People = () => {
   const [people, setPeople] = useState([]);
+  const [giftr, setGiftr] = useGiftr([]);
   const [token, setToken] = useToken();
   const navigate = useNavigate();
   
-
+  // setting to giftrContext
   useEffect(() =>{
     const api = import.meta.env.VITE_API_URL;
     const url = `${api}/people`;
@@ -28,7 +29,7 @@ const People = () => {
       })
       .then((data)=>{
         console.log(data);
-        setPeople(
+        setGiftr(
           data.map(ppl=>{
             const person = {
               id: ppl._id,
@@ -47,11 +48,39 @@ const People = () => {
       })
   },[]);
 
+  // setting to state
+  // fetch(request)
+  //     .then(res=>{
+  //       if(res.status === 401) throw new Error('Unauthorized access to API.');
+  //       if(!res.ok) throw new Error('Invalid response');
+  //       return res.json();
+  //     })
+  //     .then((data)=>{
+  //       console.log(data);
+  //       setGiftr(
+  //         data.map(ppl=>{
+  //           const person = {
+  //             id: ppl._id,
+  //             name: ppl.name,
+  //             dob: ppl.dob,
+  //             gifts: ppl.gifts
+  //           }
+  //           return person;
+  //         })
+  //       )
+  //     })
+  //     .catch(err=>{
+  //       console.warn(err.message);
+  //       setToken(null)
+  //       navigate('/');
+  //     })
+  // },[]);
+
   return (
     <section className="people">
       <h2>People</h2>
       <ul className="pplList">
-        {people.map(person=>(
+        {giftr.map(person=>(
           <ListItem key={person.id} person={person}/>
         ))}
       </ul>
