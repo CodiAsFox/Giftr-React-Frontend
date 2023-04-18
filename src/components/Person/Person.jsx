d9dee3d749fc47950ef2211ebfc6b0ca7fe6f8b4
Vincent Chevalier <chev0100@algonquinlive.com>
2023-04-18 10:09:00 -0400

    add form and pseudocode to person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+
 
 const Person = () => {
+  // are we retrieving a person from context or are we doing a getOne fetch on useEffect
+
+  // function createPerson()
+    // get input on submit
+    // create fetch
+
+  // function editPerson()
+
+  // if useParams id OR person not null, then edit person
   return (
-    <div>Person</div>
+    <section className="addPerson">
+      <form action="#">
+        <label for="persName">Name</label>
+        <input type="text" name="persName" id="persName" required></input>
+
+        <label for="dob">Date of birth</label>
+        <input type="date" name="dob" id="dob" min="2013-01-01" required></input>
+        
+        <button class="btn">Save</button>
+        <button class="btn">Delete</button>
+        <button class="btn">Cancel</button>
+      </form>
+      
+    </section>
   )
+  // else add person
 }
 
 export default Person
