57f234d901952bf7d1f7c64a73d298eef92ff3f9
Tay <rami0101@algonquinlive.com>
2023-04-20 15:56:22 -0400

    Added style fixes

--- src/components/Gifts/Gifts.jsx
+++ src/components/Gifts/Gifts.jsx
@@ -11,7 +11,10 @@ import {
   Stack,
   StackDivider,
   Text,
+  Box,
 } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGifts } from "@fortawesome/free-solid-svg-icons";
 import BoxListItem from "../BoxListItem/BoxListItem";
 
 const Gifts = () => {
@@ -139,31 +142,45 @@ const Gifts = () => {
   }, [person]);
 
   return (
-    <section className="gifts">
-      <Card>
-        <CardHeader>
-          <Heading size="md">Gift List</Heading>
+    <Box className="gifts" w="100%">
+      <Card borderRadius={10}>
+        <CardHeader bg="pink.900" borderTopRadius={10}>
+          <Heading
+            size="lg"
+            bgGradient="linear(to-r, green.200, pink.500)"
+            bgClip="text"
+            fontSize="4xl"
+            fontWeight="extrabold"
+          >
+            <FontAwesomeIcon icon={faGifts} color="#9be59d" width="3rem" />
+            <Box display="inline" pl={1}>
+              {person.name} Gift List
+            </Box>
+          </Heading>
         </CardHeader>
         <CardBody>
           <Stack divider={<StackDivider />} spacing="4">
             {!gifts[0] ? (
               <Text>
-                There's not Gifts for <strong>{person.name}</strong>!
+                <Heading size="lg">
+                  There's not Gifts for <strong>{person.name}</strong>!
+                </Heading>
+                <br />
+                Tap the button above to add one.
               </Text>
             ) : (
-              "=D"
+              gifts.map((gift) => (
+                <BoxListItem
+                  key={gift.gift_id}
+                  gift={gift}
+                  deleteItem={deleteGift}
+                />
+              ))
             )}
-            {gifts.map((gift) => (
-              <BoxListItem
-                key={gift.gift_id}
-                gift={gift}
-                deleteItem={deleteGift}
-              />
-            ))}
           </Stack>
         </CardBody>
       </Card>
-    </section>
+    </Box>
   );
 };
 
