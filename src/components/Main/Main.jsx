3a4275e52db0b58b2985a07100460032802a2d4b
Tay <rami0101@algonquinlive.com>
2023-04-20 15:56:22 -0400

    Added style fixes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,24 +5,34 @@ import Person from "../Person/Person";
 import Gifts from "../Gifts/Gifts";
 import Gift from "../Gift/Gift";
 import NotFound from "../NotFound/NotFound";
-import { Box } from "@chakra-ui/react";
+import { Box, Container, Stack } from "@chakra-ui/react";
 import { SkipNavContent } from "@chakra-ui/skip-nav";
 
 const Main = () => {
   return (
     <SkipNavContent>
-      <Box p={4}>
-        <Routes>
-          <Route path="/" element={<Home />} index />
-          <Route path="/people/" element={<People />} />
-          <Route path="/people/:id/" element={<Person />} />
-          <Route path={"/people/add"} element={<Person />} />
-          <Route path="/people/:id/gifts/" element={<Gifts />} />
-          <Route path="/people/:id/gifts/:giftId" element={<Gift />} />
-          <Route path={"/people/:id/gifts/add"} element={<Gift />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Box>
+      <Container
+        as={Stack}
+        maxW={"5xl"}
+        py={4}
+        direction={{ base: "column", md: "row" }}
+        spacing={4}
+        justify={{ base: "center", md: "space-arround" }}
+        align={{ base: "center", md: "center" }}
+      >
+        <Box p={4} w="100%">
+          <Routes>
+            <Route path="/" element={<Home />} index />
+            <Route path="/people/" element={<People />} />
+            <Route path="/people/:id/" element={<Person />} />
+            <Route path={"/people/add"} element={<Person />} />
+            <Route path="/people/:id/gifts/" element={<Gifts />} />
+            <Route path="/people/:id/gifts/:giftId" element={<Gift />} />
+            <Route path={"/people/:id/gifts/add"} element={<Gift />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Box>
+      </Container>
     </SkipNavContent>
   );
 };
