be6256fef32af2162accc3c64c92aa4a44fae6b5
Vincent Chevalier <chev0100@algonquinlive.com>
2023-04-19 22:14:22 -0400

    built patch and delete fetches

--- src/components/Gift/Gift.jsx
+++ src/components/Gift/Gift.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useToken } from '../../context/TokenContext';
 import { Button } from "@chakra-ui/react";
@@ -24,7 +24,15 @@ const Gift = () => {
   console.log('gift id: ', giftId);
   
   function saveGift() {
-    postGift(gift);
+    //PATCH
+    if (giftId) updateGift(gift);
+
+    // POST
+    if (!giftId) postGift(gift);
+  }
+
+  function doDelete() {
+    deleteGift(giftId); 
   }
   // API GET, POST, PATCH, DELETE
   const api = import.meta.env.VITE_API_URL;
@@ -50,9 +58,8 @@ const Gift = () => {
         console.log(data);
         return data;
       })
-      .then(({name, dob}) => {
-        dob = dob.split('T')[0];        
-        setPerson({name, dob});
+      .then((data) => {      
+        setGift(data);
       })
       .catch((err) => {
         console.warn(err.message);
@@ -85,6 +92,63 @@ const Gift = () => {
         navigate("/");
       });
   }
+
+  function updateGift(data) {
+    let endpoint = `${url}/${giftId}`;
+    let request = new Request(endpoint, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    fetch(request)
+      .then((res) => {
+        if (res.status === 401) throw new Error("Unauthorized access to API.");
+        if (!res.ok) throw new Error("Invalid response");
+        console.log("response: ", res);
+        return res.json();
+      })
+      .then(()=>navigate(`/people/${personId}/gifts`))
+      .catch((err) => {
+        console.warn(err.message);
+        setToken(null);
+        navigate("/");
+      });
+  }
+
+  function deleteGift(giftId) {
+    // api/people/${id}
+    let endpoint = `${url}/${giftId}`;
+    let request = new Request(endpoint, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    fetch(request)
+      .then((res) => {
+        if (res.status === 401) throw new Error("Unauthorized access to API.");
+        if (!res.ok) throw new Error("Invalid response");
+        console.log('res: ', res)
+        return res.json();
+      })
+      .then(()=>navigate(`/people/${personId}/gifts`))
+      .catch((err) => {
+        console.warn(err.message);
+        setToken(null);
+        navigate("/");
+      });
+  }
+
+  useEffect(()=>{
+    if (giftId) {
+      getGift(); // fill the inputs with data from GET fetch
+    }
+  },[]);
 
   return (
     <section className='addGift'>
@@ -137,6 +201,7 @@ const Gift = () => {
               fontSize={"sm"}
               fontWeight={600}
               mr={3}
+              onClick={doDelete}
             >
             Delete
             </Button>
