4ea6282a7dff3b63886405f7fbd368a4bd23ddf2
Vincent Chevalier <chev0100@algonquinlive.com>
2023-04-19 21:48:00 -0400

    added get one and post

--- src/components/Gift/Gift.jsx
+++ src/components/Gift/Gift.jsx
@@ -1,11 +1,148 @@
-import { useParams } from 'react-router-dom';
-import AddGift from "../AddGift/AddGift";
+import { useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useToken } from '../../context/TokenContext';
+import { Button } from "@chakra-ui/react";
 
 const Gift = () => {
-  const { id } = useParams(); // pass this down through params
+  const [token, setToken] = useToken();
+  const navigate = useNavigate();
+  // page context for navigation
+
+  const params = useParams();
+  const personId = params.id;
+  const giftId = params.giftId;
+  
+  const shape = {
+    txt: '',
+    store: '',
+    url: '',
+  };
+  
+  const [gift, setGift] = useState({...shape}); // a copy of shape is sent to STATE
+
+  console.log('person id: ', personId);
+  console.log('gift id: ', giftId);
+  
+  function saveGift() {
+    postGift(gift);
+  }
+  // API GET, POST, PATCH, DELETE
+  const api = import.meta.env.VITE_API_URL;
+  const url = `${api}/people/${personId}/gifts`;
+
+  function getGift() {
+    // api/people/${id}/gifts/${giftId}
+    let endpoint = `${url}/${giftId}`;
+    let request = new Request(endpoint, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    fetch(request)
+      .then((res) => {
+        if (res.status === 401) throw new Error("Unauthorized access to API.");
+        if (!res.ok) throw new Error("Invalid response");
+        return res.json();
+      })
+      .then(({ data }) => {
+        console.log(data);
+        return data;
+      })
+      .then(({name, dob}) => {
+        dob = dob.split('T')[0];        
+        setPerson({name, dob});
+      })
+      .catch((err) => {
+        console.warn(err.message);
+        setToken(null);
+        navigate("/");
+      });
+  }
+  
+  function postGift(data) {
+    let request = new Request(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data),
+    });
+
+    fetch(request)
+      .then(res=>{
+        if (res.status === 401) throw new Error("Unauthorized access to API.");
+        if (!res.ok) throw new Error("Invalid response");
+        console.log('response: ', res);
+        return res.json();
+      })
+      .then(()=>navigate(`/people/${personId}/gifts`))
+      .catch((err) => {
+        console.warn(err.message);
+        setToken(null);
+        navigate("/");
+      });
+  }
+
   return (
-    <section className="gift">
-      <AddGift personId={id}/>
+    <section className='addGift'>
+      <form>
+          <div className="formBox">
+            <label htmlFor="gift_idea">Gift Idea</label>
+            <input 
+              type="text" 
+              name="gift_idea" 
+              maxLength="80"
+              placeholder="Enter your gift idea"
+              value={`${gift.txt}`} 
+              onChange={(ev) => setGift({ ...gift, txt: ev.target.value })}
+              required />
+          </div>
+          <div className="formBox">
+            <label htmlFor="store">Store</label>
+            <input 
+            type="text" 
+            name="store" 
+            maxLength="80"  
+            value={`${gift.store}`} 
+            onChange={(ev) => setGift({ ...gift, store: ev.target.value })} 
+            required />
+          </div>
+          <div className="formBox">
+            <label htmlFor="url">URL</label>
+            <input 
+            type="text" 
+            name="url" 
+            maxLength="80"  
+            value={`${gift.url}`} 
+            onChange={(ev) => setGift({ ...gift, url: ev.target.value })} 
+            required />
+          </div>
+          <div className="formBox">
+            <Button
+              colorScheme="teal"
+              as={"a"}
+              fontSize={"sm"}
+              fontWeight={600}
+              mr={3}
+              onClick={saveGift}
+            >
+            Save
+            </Button>
+            <Button
+              colorScheme="teal"
+              as={"a"}
+              fontSize={"sm"}
+              fontWeight={600}
+              mr={3}
+            >
+            Delete
+            </Button>
+            
+          </div>
+        </form>
     </section>
   )
 }
